ff51bee35be395ae313c93e349566437b3504773
Marin <niramnagrab@gmail.com>
2021-08-14 19:32:06 +0800

    Changed link

--- src/components/Payments/index.js
+++ src/components/Payments/index.js
@@ -174,10 +174,7 @@ const Payments = () => {
             <p>Total : RMB {total - discountValue}</p>
           </div>
           <div className="course-btn">
-            <form
-              action="http://englishinspiredbyfootballsp.com/checkout/index.php"
-              method="POST"
-            >
+            <form action="checkout/index.php" method="POST">
               <input type="hidden" name="price" value={price} />
               <input type="hidden" name="quantity" value={quantity} />
               <input type="hidden" name="price" value={total - discountValue} />
